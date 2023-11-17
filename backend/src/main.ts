dd33571463a3f1e9846ab73b8c97d96d659eec7e
Sergey Elpashev <elpashevs@bk.ru>
2023-11-18 02:00:33 +0300

    Added swagger and config read

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { config } from 'config';
 import { AppModule } from './app.module';
+import { swagger } from './swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, {
+        logger: ['log', 'debug', 'error', 'warn', 'verbose'],
+    });
+    swagger(app);
+    await app.listen(config.server.port, () => Logger.log(`Server started on port ${config.server.port}`));
 }
 bootstrap();
