6bf68868262671f6110e97f105200dff3655d28e
Errormacr <tulupov.valeriy@mail.ru>
2023-11-18 19:04:33 +0300

    Delete useless

--- backend/src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const userRep: string = 'USER_REPOSITORY';
-export const dataSource: string = 'DATA_SOURCE';
-export const adminRep: string = 'ADMIN_REPOSITORY';
-export const photoRep: string = 'PHOTO_REPOSITORY';
-export const postRep: string = 'POST_REPOSITORY';
