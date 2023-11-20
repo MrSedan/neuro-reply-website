fcf15d668f1b8d72c89d4af070f241aef6a2d85a
Sergey Elpashev <elpashevs@bk.ru>
2023-11-20 19:10:23 +0300

    Added post module to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'config';
 import { LibsModule } from 'libs/libs.module';
-import { AppInitService } from './modules/initialization/app.init.service';
-import { UserModule } from './modules/user/user.module';
 import { AdminModule } from './modules/admin/admin.module';
+import { AppInitService } from './modules/initialization/app.init.service';
+import { PostModule } from './modules/post/post.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-    imports: [AdminModule, LibsModule, UserModule, TypeOrmModule.forRoot(<TypeOrmModuleOptions>config.database)],
+    imports: [LibsModule, PostModule, AdminModule, UserModule, TypeOrmModule.forRoot(<TypeOrmModuleOptions>config.database)],
     controllers: [],
     providers: [AppInitService],
 })
