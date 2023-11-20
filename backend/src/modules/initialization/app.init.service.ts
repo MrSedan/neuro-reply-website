dbd0fb51b79a2edf479fe537ddf47d8a5e2fb22a
Sergey Elpashev <elpashevs@bk.ru>
2023-11-20 19:08:16 +0300

    Added new repos to init

--- backend/src/modules/initialization/app.init.service.ts
+++ backend/src/modules/initialization/app.init.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Admin } from 'libs/database/admin.entity';
+import { Image } from 'libs/database/image.entity';
+import { Post } from 'libs/database/post.entity';
 import { User } from 'libs/database/user.entity';
 import { Repository } from 'typeorm';
 
@@ -9,6 +11,8 @@ export class AppInitService implements OnModuleInit {
     constructor(
         @InjectRepository(User) private userRepository: Repository<User>,
         @InjectRepository(Admin) private adminRepository: Repository<Admin>,
+        @InjectRepository(Post) private postRepository: Repository<Post>,
+        @InjectRepository(Image) private ImageRepository: Repository<Image>,
     ) {}
 
     async onModuleInit() {}
