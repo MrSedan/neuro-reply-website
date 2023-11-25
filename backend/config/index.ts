da93e2a4b25267fe62b75092c401379fb56f6f62
Errormacr <tulupov.valeriy@mail.ru>
2023-11-25 18:59:13 +0300

    fix some problem, can't connect

--- backend/config/index.ts
+++ backend/config/index.ts
@@ -8,7 +8,7 @@ export const config = {
         host: process.env.DATABASE_HOST || 'localhost',
         port: +process.env.DATABASE_PORT || 5432,
         username: process.env.DATABASE_USERNAME || 'postgres',
-        password: process.env.DATABASE_PASSWORD || '',
+        password: process.env.DATABASE_PASSWORD || 'postgres',
         database: process.env.DATABASE_DB || 'bot_db',
         synchronize: true,
         logging: false,
