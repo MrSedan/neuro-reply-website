5a340a2065e44e1480a511ff8b9ae68262fa835c
Sergey Elpashev <elpashevs@bk.ru>
2023-12-20 12:08:32 +0300

    Added settings entity

--- backend/libs/libs.module.ts
+++ backend/libs/libs.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './database/user.entity';
 import { Admin } from './database/admin.entity';
-import { Post } from './database/post.entity';
 import { Image } from './database/image.entity';
 import { Payment } from './database/payment.entity';
+import { Post } from './database/post.entity';
 import { ProxyUser } from './database/proxy_user.entity';
+import { BotSettings } from './database/settings.entity';
+import { User } from './database/user.entity';
 @Module({
-    imports: [TypeOrmModule.forFeature([User, Admin, Post, Image, Payment, ProxyUser])],
+    imports: [TypeOrmModule.forFeature([User, Admin, Post, Image, Payment, ProxyUser, BotSettings])],
     exports: [TypeOrmModule],
 })
 export class LibsModule {}
